7ef10a4189dfeed5fa3be7fdac93c20a4c98fa0f
aharis <hare.arapovic@gmail.com>
2022-06-22 09:24:00 +0200

    list model

---
+++ models/listModel/list.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+
+const listSchema = mongoose.Schema({
+
+    listName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    userId: {
+        type: String,
+        required: true
+    },
+    productList: [
+        {
+            productName: {
+                type: String
+            },
+            quantity: {
+                type: Number
+            }
+        }
+    ]
+
+},
+    { timestamps: true }
+);
+
+const List = mongoose.model("List", listSchema);
+
+export default List;
