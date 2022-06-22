c4d27b38ad207d8887aa1df83e31927a7339f6ea
aharis <hare.arapovic@gmail.com>
2022-06-22 13:18:04 +0200

    delete list

--- controllers/listControllers/list.controller.js
+++ controllers/listControllers/list.controller.js
@@ -3,10 +3,10 @@ import List from "../../models/listModel/list.model.js"
 export const createList = async (req, res) => {
     const { listName, userId, productList } = req.body;
     const existList = await List.findOne({ listName });
-    if(existList) {
+    if (existList) {
         return res.status(404).json("List Alredy Exist")
     }
-    if(!userId) {
+    if (!userId) {
         return res.status(404).json("User Id is requred!")
     }
 
@@ -15,30 +15,35 @@ export const createList = async (req, res) => {
         userId,
         productList
     });
-    if(list) {
+    if (list) {
         res.status(201).json({
             _id: list._id,
             listName: list.listName,
             userId: list.userId,
             productList: list.productList
         })
-    }else {
+    } else {
         res.status(400).json("List creation failed")
     }
- }
+}
 
 
- export const updateList =async (req, res) => {
+export const updateList = async (req, res) => {
     try {
-        const updatedList = await List.findByIdAndUpdate(req.params.id, {$set: req.body}, {new:true})
-        console.log(updatedList)
+        const updatedList = await List.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
         return res.status(201).json(updatedList)
     } catch (error) {
         res.status(500).json("Updated fail")
     }
- }
+}
 
 
-export const deleteList = () => {
-
+export const deleteList = async (req, res) => {
+    let { id } = req.params;
+    try {
+        await List.findByIdAndDelete(id)
+        return res.status(200).json('List successfully deleted !')
+    } catch (error) {
+        res.status(500).json('List delete fail!')
+    }
 }
