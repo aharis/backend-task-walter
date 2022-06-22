30276f65ea3bf0c8c260c242f45b51ff51442edd
aharis <hare.arapovic@gmail.com>
2022-06-22 12:01:58 +0200

    list controller

--- controllers/listControllers/list.controller.js
+++ controllers/listControllers/list.controller.js
@@ -1,8 +1,32 @@
-import Model from "../../models/listModel/list.model.js"
+import List from "../../models/listModel/list.model.js"
 
-export const createList = () => {
+export const createList = async (req, res) => {
+    const { listName, userId, productList } = req.body;
+    const existList = await List.findOne({ listName });
+    if(existList) {
+        return res.status(404).json("List Alredy Exist")
+    }
+    if(!userId) {
+        return res.status(404).json("User Id is requred!")
+    }
 
-}
+    const list = await List.create({
+        listName,
+        userId,
+        productList
+    });
+    if(list) {
+        res.status(201).json({
+            _id: list._id,
+            listName: list.listName,
+            userId: list.userId,
+            productList: list.productList
+        })
+    }else {
+        res.status(400).json("List creation failed")
+    }
+ }
+
 
 export const updateList = () => {
 
